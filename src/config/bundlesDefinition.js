65b7c29df743bc1b680cec0bb522450493860e04
MadDeveloper <julien.sergent46@orange.fr>
2016-05-17 17:29:25 +0200

    Refactoring

--- src/config/bundlesDefinition.js
+++
@@ -1,9 +0,0 @@
-export default function bundlesDefinition( bundleManager ) {
-    /*
-     * Register bundles to implements routing
-     */
-    bundleManager
-        .define( 'role' )
-        .define( 'user' )
-    //  .define( 'skeleton' ) // uncomment this line if you want verify Skeleton files syntax
-}
--- src/config/database/connector/mongodb.js
+++
@@ -1,9 +0,0 @@
-/*
- * Not working!
- */
-import mongoose from 'mongoose'
-import config   from './../../config'
-
-const params = config.database
-
-export default mongoose.connect( 'mongodb://' + params.connection.host + '/' + params.connection.database )
