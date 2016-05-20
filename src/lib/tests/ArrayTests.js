b989bbecb613eab54eb54df121fd98829f8ab94a
MadDeveloper <julien.sergent46@orange.fr>
2016-05-20 17:01:22 +0200

    Refactoring

---
+++ src/lib/tests/ArrayTests.js
@@ -0,0 +1,32 @@
+/*
+ * Units tests
+ */
+import test     from 'unit.js'
+import chai     from 'chai'
+import Tests    from './../../vendor/easy/core/Tests'
+
+const assert    = chai.assert
+const expect    = chai.expect
+
+class ArrayTests extends Tests {
+    constructor( container ) {
+        super( container )
+    }
+
+    /*
+     * Automatically called
+     */
+    run() {
+        describe( 'Array.js library tests', () => {
+          it( 'treeView tests', () => {
+
+
+          })
+        })
+    }
+}
+
+/*
+ * Permit mocha to access to run() method called implicitely and run unit tests
+ */
+new ArrayTests( global.container )
