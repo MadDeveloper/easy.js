10ca754167c0dc401569d68c0e21ba3d00373ecc
jsergent <julien.a.sergent@capgemini.com>
2016-12-30 17:00:53 +0100

    Clean role tests

--- src/bundles/role/tests/RoleTests.spec.js
+++ src/bundles/role/tests/RoleTests.spec.js
@@ -1,38 +1,57 @@
-// const Tests = require( 'easy/core/Tests' )
-// const RoleController = require( '../' ).controller
-//
-// /**
-//  * @class RoleTests
-//  * @extends Tests
-//  */
-// class RoleTests extends Tests {
-//     /*
-//      * Automatically called
-//      */
-//     run() {
-//         this.roleController = new RoleController( easy.application.container )
-//
-//         /*
-//          * Execute all tests
-//          */
-//         this.getRolesTest()
-//     }
-//
-//     /**
-//      * getRolesTest - description
-//      *
-//      * @returns {type}  description
-//      */
-//     getRolesTest() {
-//         describe( "Test getRoles", () => {
-//             it ( "all roles", () => {
-//                 this.roleController.getRoles( this.request, this.response )
-//             })
-//         })
-//     }
-// }
-//
-// /*
-//  * Permit Jasmine to access to run() method called implicitely and run units tests
-//  */
-// new RoleTests()
+const RoleController = require( '../' ).controller
+// const { entityManager, response, request } = require( 'easy/test/mocks' )
+
+function fakeAsync(cb) {
+    return function(done) {
+        cb()
+        setTimeout(done, 0)
+    }
+}
+
+describe('RoleController', () => {
+
+    let container = easy.application.container
+    let response, mockEntityManager
+    let roleController
+
+    beforeEach(() => {
+        response = jasmine.createSpyObj('response', ['ok', 'internalServerError'])
+    })
+
+    beforeEach(() => {
+        mockEntityManager = jasmine.createSpyObj( 'em', [ 'getRepository' ] )
+        container.register( 'component.entitymanager', mockEntityManager )
+        roleController = new RoleController( container )
+    })
+
+    describe('getRoles', () => {
+
+        describe('when the repository respond successfully', () => {
+
+            let roles = [ { k: 'bar' }, { k: 'foo' } ]
+
+            beforeEach( () => mockEntityManager.getRepository.and.returnValue({ findAll: () => Promise.resolve( roles ) }) )
+
+            beforeEach(fakeAsync(() => roleController.getRoles( {}, response ) ))
+
+            it( 'should respond with the provided roles', () => {
+                expect( response.ok ).toHaveBeenCalledWith( roles )
+            })
+
+        })
+
+        describe('when the repository respond with an error', () => {
+
+            beforeEach(() => mockEntityManager.getRepository.and.returnValue( { findAll: () => Promise.reject( 'Something terrible happened!' ) } ) )
+
+            beforeEach(fakeAsync(() => roleController.getRoles( {}, response ) ))
+
+            it( 'should respond with an error', () => {
+                expect( response.internalServerError ).toHaveBeenCalledWith( 'Something terrible happened!' )
+            })
+
+        })
+
+    })
+
+})
