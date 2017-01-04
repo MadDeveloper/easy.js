d68f410b17ba96a7795eb4859b4e1bb2d3876767
MadDeveloper <sergent.julien@icloud.com>
2017-01-05 00:41:50 +0100

    Remove mongoose connector

--- src/config/database/connectors/mongoose.js
+++
@@ -1,6 +0,0 @@
-const mongoose      = require( 'mongoose' )
-const { config }    = require( 'knex' )
-
-mongoose.connect( `mongodb://${config.host}/${config.database}` )
-
-module.exports = mongoose
