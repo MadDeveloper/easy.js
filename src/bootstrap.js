67fbb555178dc350e850a40c0942eebbbd4f7972
MadDeveloper <sergent.julien@icloud.com>
2017-03-01 00:43:49 +0100

    refactor: Remove app-module-path package

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,19 +1,12 @@
 require( 'use-strict' )
 
 /*
- * Permit to require from application root path
- */
-const appModulePath = require( 'app-module-path' )
-const appRootPath = `${__dirname}/../`
-
-appModulePath.addPath( appRootPath )
-
-/*
  * Import and configure main Easy class
  */
 const { Application } = require( 'easy/core' )
-
+const appRootPath = `${__dirname}/../`
 const application = new Application()
+
 application.configure( appRootPath )
 
-module.exports = { kernel: application.kernel, application }
+module.exports = { application }
